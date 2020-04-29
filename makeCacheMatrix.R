9364363342829c03b3c62581f11d1fb54a360b6d
mmstonge <63751420+mmstonge@users.noreply.github.com>
2020-04-29 10:51:35 -0700

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,16 @@
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
